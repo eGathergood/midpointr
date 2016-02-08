34ed44e2fbcf0fe8702d39b4e8e8e5801418feff
cgathergood <calumgathergood@gmail.com>
2016-02-08 22:30:47 +0000

    Added in geolocation

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 function initMap() {
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
-      center: {lat: 44.540, lng: -78.546},
-      zoom: 8
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 6
     });
-  }
+
+    // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+  	navigator.geolocation.getCurrentPosition(function(position) {
+  		var pos = {
+  			lat: position.coords.latitude,
+  			lng: position.coords.longitude
+  		};
+  		map.setCenter(pos);
+  	}, function() {
+  		handleLocationError(true, map.getCenter());
+  	});
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, map.getCenter());
+}
+}
+
+function handleLocationError(browserHasGeolocation, pos) {
+	console.log("GPS not supported.")
+}
