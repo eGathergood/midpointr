741325ffd942d779ec57964c07c726f8a15df5eb
cgathergood <calumgathergood@gmail.com>
2016-02-08 22:33:09 +0000

    Added info window on users location

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 function initMap() {
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 6
-    });
+	var mapDiv = document.getElementById('map');
+	var map = new google.maps.Map(mapDiv, {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 6
+	});
+
+	var infoWindow = new google.maps.InfoWindow({map: map});
 
     // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-  	navigator.geolocation.getCurrentPosition(function(position) {
-  		var pos = {
-  			lat: position.coords.latitude,
-  			lng: position.coords.longitude
-  		};
-  		map.setCenter(pos);
-  	}, function() {
-  		handleLocationError(true, map.getCenter());
-  	});
-  } else {
+    if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+    		var pos = {
+    			lat: position.coords.latitude,
+    			lng: position.coords.longitude
+    		};
+
+    		infoWindow.setPosition(pos);
+    		infoWindow.setContent('You are here!');
+    		map.setCenter(pos);
+    	}, function() {
+    		handleLocationError(true, map.getCenter());
+    	});
+    } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, map.getCenter());
 }
